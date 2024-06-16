1d8e77c4d1cabfd1317f074bc31ce272fdce3fc6
royalaliyevdev <royaldadashaliyev@gmail.com>
2024-06-16 20:11:33 +0400

    Without token coupon and store dashboard - 01

--- coupon.php
+++ coupon.php
@@ -1,18 +1,17 @@
 <?php
-require '../config.php';
+require 'config.php';
 
-    if ($coupon_number !== false) {
-        $sql = "SELECT * FROM coupons WHERE coupon_number = $coupon_number";
-        $result = $conn->query($sql);
+if (isset($_GET['coupon_number'])) {
+    $coupon_number = $_GET['coupon_number'];
+    $file_path = "generated_coupons/coupon_$coupon_number.jpg";
 
-        if ($result->num_rows > 0) {
-            $coupon = $result->fetch_assoc();
-            echo json_encode(['success' => true, 'coupon' => $coupon]);
-        } else {
-            echo json_encode(['success' => false, 'message' => 'Coupon not found in database']);
-        }
+    if (file_exists($file_path)) {
+        header('Content-Type: image/jpeg');
+        readfile($file_path);
+    } else {
+        echo "Coupon not found.";
     }
- else {
-    echo json_encode(['success' => false, 'message' => 'Invalid request']);
-    }
+} else {
+    echo "Invalid request.";
+}
 ?>
